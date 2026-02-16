f10960f524cc7cdbac103dadfc18e461786df60e
Antonio Herrera <herreraquinteroj@gmail.com>
2026-02-16 12:14:12 -0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://vertexf.com/',
+  integrations: [sitemap()],
   vite: {
     server: {
       fs: {
