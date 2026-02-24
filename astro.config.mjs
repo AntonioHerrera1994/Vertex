44afe43d7eee2ddb0090088ef8ad05136814ad22
Antonio Herrera <herreraquinteroj@gmail.com>
2026-02-24 15:15:55 -0800

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://vertexf.com/',
+  site: 'https://veloracargo.com',
   integrations: [sitemap()],
   vite: {
     server: {
